95f9aba9a8bdbedd7f38d06252e061f9d63d93bd
youngchild-cloud <rladudcks8017@gmail.com>
2026-01-28 16:26:15 +0900

    자유게시판 출력중

--- routes/community.routes.js
+++ routes/community.routes.js
@@ -19,4 +19,23 @@ router.get('/communitylist', (req, res) => {
   });
 });
 
+// 자유게시판 상세 페이지 조회
+router.get('/community/detail/:bc_no', (req, res) => {
+  const bc_no = req.params.bc_no;
+  connection.query(
+    `SELECT board_community.*, users.*
+    FROM board_community
+    INNER JOIN users ON board_community.bc_user_no = users.u_no
+    WHERE board_community.bc_no = ? `,
+    [bc_no],
+    (err, results) => {
+      if (err) {
+        console.log('조회 오류 : ', err);
+        return res.status(500).json({ error: '조회 실패' });
+      }
+      return res.json(results);
+    }
+  )
+})
+
 module.exports = router;
