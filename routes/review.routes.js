2f6bcd90b696a58cc705d59470f231623231d83e
hrkim0413 <hrkim041396@gmail.com>
2026-01-28 17:41:16 +0900

    맛집 리뷰 > 데이터 연결 작업 중

--- routes/review.routes.js
+++ routes/review.routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const connection = require('../config/db');
 
-// 맛집 리뷰 review - 목록 조회
+// 맛집 리뷰 - 목록 조회 review
 router.get('/review', (req, res) => {
   connection.query(
     'SELECT * FROM board_review ORDER BY br_no DESC',
@@ -14,5 +14,56 @@ router.get('/review', (req, res) => {
     }
   );
 });
+
+// 맛집 리뷰 - 상세 조회 review/detail
+router.post('/review/detail/', (req, res) => {
+  const { br_no } = req.body;
+
+  connection.query(
+    'SELECT * FROM board_review WHERE br_no = ?',
+    [br_no],
+    (err, result) => {
+      if (err) {
+        console.log('DB ERROR:', err);
+        return res.status(500).json({ error: err.message });
+      }
+      res.json(result[0]);
+    }
+  )
+})
+
+// 맛집 - 목록 조회 review/restaurant
+router.post('/restaurant', (req, res) => {
+  const { cate } = req.body;
+
+  connection.query(
+    'SELECT * FROM restaurant WHERE rt_cate = ? ORDER BY rt_rank',
+    [cate],
+    (err, result) => {
+      if (err) {
+        console.log('DB ERROR:', err);
+        return res.status(500).json({ error: err.message });
+      }
+      res.json(result);
+    }
+  );
+});
+
+// 맛집 - 상세 조회 review/restaurant/detail
+router.get('/restaurant/detail/:rt_no', (req, res) => {
+  const rt_no = req.params.rt_no;
+
+  connection.query(
+    'SELECT * FROM restaurant WHERE rt_no = ?',
+    [rt_no],
+    (err, result) => {
+      if (err) {
+        console.log('DB ERROR:', err);
+        return res.status(500).json({ error: err.message });
+      }
+      res.json(result[0]);
+    }
+  )
+})
 
 module.exports = router;
