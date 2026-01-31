7bb332bb11ac36a66336d2537df9796174dd6c1c
hrkim0413 <hrkim041396@gmail.com>
2026-01-31 18:13:50 +0900

    로그인 > db 출력 개발

--- routes/login.routes.js
+++ routes/login.routes.js
@@ -49,5 +49,42 @@ router.post('/join', upload.single('u_pic'), async (req, res) => {
 });
 
 // 로그인
+router.post('/login', (req, res) => {
+  const { u_id, u_pw } = req.body;
+
+  connection.query(
+    'SELECT * FROM users WHERE u_id = ?',
+    [u_id],
+    async (err, result) => {
+      if (err) {
+        return res.status(500).json({ error: '서버 오류' });
+      }
+
+      if (result.length === 0) {
+        return res.status(404).json({ error: '존재하지 않는 아이디입니다. 재확인 후 다시 시도해주세요.' });
+      }
+
+      const user = result[0];
+      const isMatch = await bcrypt.compare(u_pw, user.u_pw);
+
+      if (!isMatch) {
+        return res.status(401).json({ error: '비밀번호가 틀렸습니다. 재확인 후 다시 시도해주세요.' });
+      }
+
+      const token = jwt.sign(
+        {
+          token_id: user.u_id,
+          token_nick: user.u_nick,
+          token_profile: user.u_pic,
+          token_badge: user.u_badge,
+        },
+        SECRET_KEY,
+        { expiresIn: '1h' }
+      );
+
+      res.json({ token });
+    }
+  )
+})
 
 module.exports = router;
